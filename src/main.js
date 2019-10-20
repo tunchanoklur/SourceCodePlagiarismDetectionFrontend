82c34c2a196d126da29fb62d2e8d4bcb36a23a41
kikk <kikpanda@gmail.com>
2019-10-20 21:29:27 +0700

    resolve cors error by changing path

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import "material-design-icons-iconfont/dist/material-design-icons.css";
 import axios from "axios";
 
 Vue.config.productionTip = false;
-axios.defaults.baseURL = "localhost:5000";
+axios.defaults.baseURL = "http://127.0.0.1:5000";
 
 new Vue({
   router,
