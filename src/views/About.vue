1e8560863673f46893fc7262359b5309ceb8e422
kikk <kikpanda@gmail.com>
2019-10-28 16:39:51 +0700

    About page explanation

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-card>
+      <v-card-title class="headline font-weight-regular black white--text">
+        <span style="font-size:32px">
+          <b>About</b>
+        </span>
+      </v-card-title>
+      <v-card-text>
+        <br />
+        <h2>This project is submitted to fulfill the requirements for the degree of bachelor of Computer Engineering, Mahidol University International College.</h2>
+        <br />
+        <h2>Under the supervision of :<br /></h2>
+        <h2><ol><li>Dr. Vasin Sutthichaya</li><li>Dr. Mingmanas Sivaraksa</li><li>Asst. Prof. Dr. Tanasanee  Phienthrakul</li></ol></h2>
+        <br />
+        <h2>Submitted by :</h2>
+        <h2><ol><li>Ms. Niracha Eakvorachai</li><li>Ms. Tunchanok Lurkraisit</li></ol></h2>
+        <br />
+        <h2>The purpose of this study is to create a model to eliminate the problems of source code plagiarism in academic area. However,
+           the definition of plagiarism varies upon each person perspective. We suggest not to only rely on similarity score, 
+           but lines of code should be taken into consideration to trace back the similar section.</h2>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
